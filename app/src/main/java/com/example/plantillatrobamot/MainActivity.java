4f23aac29ff81f7fd1d387a39f9e7340f4384621
alber <albertsalomv@gmail.com>
2023-05-18 16:29:29 +0200

    actualizacion

--- app/src/main/java/com/example/plantillatrobamot/MainActivity.java
+++ app/src/main/java/com/example/plantillatrobamot/MainActivity.java
@@ -32,8 +32,6 @@ public class MainActivity extends AppCompatActivity {
     // Variables de lògica del joc
     private final int lengthWord = 5;
     private final int maxTry = 6;
-    gjfjgkdjdfhdfd
-
     private String palabraSolucion = "pasta";
     private int numIntentos = 0;
     private int numLetra = 0;
